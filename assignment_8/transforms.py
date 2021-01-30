440fb90dbe5f1ff77d8b10ab9af815f804348bf6
dexter11235813 <abgairola@gmail.com>
2021-01-30 22:04:03 +0530

    removed jitter transform

--- assignment_8/transforms.py
+++ assignment_8/transforms.py
@@ -2,12 +2,11 @@ import torchvision.transforms as transforms
 
 train_transforms = transforms.Compose(
     [
-        transforms.ColorJitter(0.25, 0.2, 0.3),
         transforms.RandomRotation(2),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(
-            mean=[0.4914, 0.48216, 0.44653], std=[0.24703, 0.24349, 0.26159]
+            mean=[0.4914, 0.48216, 0.44653], std=[0.2023, 0.1994, 0.2010]
         ),
     ]
 )
@@ -16,7 +15,7 @@ test_transforms = transforms.Compose(
     [
         transforms.ToTensor(),
         transforms.Normalize(
-            mean=[0.4914, 0.48216, 0.44653], std=[0.24703, 0.24349, 0.26159]
+            mean=[0.4914, 0.48216, 0.44653], std=[0.2023, 0.1994, 0.2010]
         ),
     ]
 )
