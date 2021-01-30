dba61fab32b5b111cd774c4a84d5b1e441dd5210
dexter11235813 <abgairola@gmail.com>
2021-01-30 19:57:47 +0530

    first push

---
+++ assignment_8/transforms.py
@@ -0,0 +1,9 @@
+import torchvision.transforms as transforms
+
+transform = transforms.Compose(
+    [
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+)
