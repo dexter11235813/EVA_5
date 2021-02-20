672089017e1a219198a7c50060a52b125180e68f
dexter11235813 <abgairola@gmail.com>
2021-02-20 20:01:57 +0530

    changed fn definition

--- assignment_10/find_best_lr.py
+++ assignment_10/find_best_lr.py
@@ -7,16 +7,16 @@ from model import ResNet18
 from torch.optim.lr_scheduler import OneCycleLR
 
 
-def find_best_lr(use_val_loader=False):
+def find_best_lr(use_val_loader=False, start_lr=1e-5, end_lr=0.1):
     net = ResNet18().to(config.DEVICE)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=1e-5, momentum=0.9)
+    optimizer = optim.SGD(net.parameters(), lr=start_lr, momentum=0.9)
     lr_finder = LRFinder(net, optimizer, criterion, device=config.DEVICE)
     train_loader, test_loader = dataloader.get_iterators()
     if not use_val_loader:
-        lr_finder.range_test(train_loader, end_lr=0.1)
+        lr_finder.range_test(train_loader, end_lr=end_lr)
     else:
-        lr_finder.range_test(train_loader, val_loader=test_loader, end_lr=0.1)
+        lr_finder.range_test(train_loader, val_loader=test_loader, end_lr=end_lr)
     lr_finder.plot(log_lr=False)
     lr_finder.reset()
 
