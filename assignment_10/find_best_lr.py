6d2f75fd845ad3bd5c8244a11f751d9d5eb6065b
dexter11235813 <abgairola@gmail.com>
2021-02-20 23:46:56 +0530

    added more parameters tro find_best_lr

--- assignment_10/find_best_lr.py
+++ assignment_10/find_best_lr.py
@@ -4,21 +4,34 @@ import torch.optim as optim
 import dataloader
 import config
 from model import ResNet18
-from torch.optim.lr_scheduler import OneCycleLR
 
 
-def find_best_lr(use_val_loader=False, start_lr=1e-5, end_lr=0.1):
+def find_best_lr(
+    use_val_loader=False, start_lr=1e-5, end_lr=0.1, step_mode="linear", num_iter=100
+):
     net = ResNet18().to(config.DEVICE)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=start_lr, momentum=0.9)
     lr_finder = LRFinder(net, optimizer, criterion, device=config.DEVICE)
     train_loader, test_loader = dataloader.get_iterators()
     if not use_val_loader:
-        lr_finder.range_test(train_loader, start_lr=start_lr, end_lr=end_lr)
+        lr_finder.range_test(
+            train_loader,
+            start_lr=start_lr,
+            end_lr=end_lr,
+            num_iter=num_iter,
+            step_mode=step_mode,
+        )
     else:
         lr_finder.range_test(
-            train_loader, val_loader=test_loader, start_lr=start_lr, end_lr=end_lr
+            train_loader,
+            val_loader=test_loader,
+            start_lr=start_lr,
+            end_lr=end_lr,
+            num_iter=num_iter,
+            step_mode=step_mode,
         )
     lr_finder.plot(log_lr=False)
     lr_finder.reset()
+    return lr_finder
 
