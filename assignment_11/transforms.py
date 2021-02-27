28fb1c7948ebc93e6da7a21080fde4898a5da0b1
dexter11235813 <abgairola@gmail.com>
2021-02-27 21:31:38 +0530

    added pad transform before random crop

--- assignment_11/transforms.py
+++ assignment_11/transforms.py
@@ -1,3 +1,4 @@
+from albumentations.augmentations.transforms import PadIfNeeded
 import torchvision.transforms as transforms
 from numpy import array
 import albumentations
@@ -8,10 +9,13 @@ class TrainTransforms:
     def __init__(self):
         self.train_transforms = albumentations.Compose(
             [
-                albumentations.RandomCrop(height=32, width=32),
+                albumentations.PadIfNeeded(
+                    min_height=36, min_width=36, always_apply=True
+                ),
+                albumentations.RandomCrop(height=32, width=32, always_apply=True),
                 albumentations.HorizontalFlip(),
                 # albumentations.Rotate(2),
-                albumentations.Cutout(num_holes=2),
+                albumentations.Cutout(num_holes=2, p=0.6),
                 albumentations.Normalize(
                     mean=[0.4914, 0.48216, 0.44653], std=[0.2023, 0.1994, 0.2010]
                 ),
